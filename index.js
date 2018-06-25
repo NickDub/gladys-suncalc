93929397df997fca2a8ba208ca19ef0492e107a0
Dubois Nicolas <dubois.nicolas@free.fr>
2018-06-25 19:53:47 +0200

    add install function

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = function(sails) {
-
+	var install = require('./lib/install.js');
 	var sunPos = require('./lib/sun/position.js');
 	var moonPos = require('./lib/moon/position.js');
 	var moonIllu = require('./lib/moon/illumination.js');
 
 	return {
+		install : install,
 		sun : {
 			position : sunPos
 		},
